e33be559e4ce56e2993c74a735d6643446004d7a
Shaikh Rehan <104453228+whozRehuCodes@users.noreply.github.com>
2022-11-02 18:10:43 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-HI this is Sample react app deployment on AWS amplify        </p>
+HI this is Sample react app deployment on AWS amplify.        </p>
         <a
           className="App-link"
           href="https://unity.e-zest.in/web#action=773&cids=&menu_id=95"
