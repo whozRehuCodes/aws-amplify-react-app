4d0b98378f41ebc08932f117667e611184dd9a6c
whozRehuCodes <104453228+whozRehuCodes@users.noreply.github.com>
2022-11-02 18:00:11 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          bla bla bla
         </p>
         <a
           className="App-link"
